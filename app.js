f9440ec412bedaa022e93bc0c790063a14aa4f22
Santhosh-Charanthu <santhoshcharanthu455@gmail.com>
2025-02-17 19:39:56 +0530

    Added

---
+++ app.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const app = express();
+const path = require("path");
+const mongoose = require("mongoose");
+const methodOverride = require("method-override");
+const ejsMate = require("ejs-mate");
+
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+app.use(express.urlencoded({ extended: true }));
+app.use(methodOverride("_method"));
+app.engine("ejs", ejsMate);
+app.use(express.static(path.join(__dirname, "/public")));
+
+app.get("/", (req, res) => {
+  res.render("includes/dashboard.ejs");
+});
+
+app.get("/");
+
+app.listen("8080", () => {
+  console.log("Listening to port 8080");
+});
